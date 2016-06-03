9fd83ce61abc13bd045c2443767f51cfc85277dc
Vincent Pauley <vinniepauley@gmail.com>
2016-06-02 22:51:38 -0500

    finished simple CLI to mongo app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# simple node stuff" 
+"for basic node to mongo connection, view the first commit with app.js,
+
+project was expanded upon to be a simple CLI app that takes an input of an original 6 hockey team at the command line, then queries the database and returns what city that team plays in." 
--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-// require the mongo client from MongoDB
+/* MODULES */
+var commandLineArgs = require('command-line-args');
 var MongoClient = require('mongodb').MongoClient;
 
-// connect to database
+/* set up the argument for team name */
+var optionDefinitions = [
+  {name: 'team', alias: 't', type: String}
+]
+
+/* parse input into 'options' var */
+var options = commandLineArgs(optionDefinitions);
+
+/* What will go into the DB query */
+var team = options.team;
+
+/* Connect to database */
 MongoClient.connect('mongodb://localhost:27017/hockey', function(err, db) {
   if(err) {
     console.log('error');
   } else {
     var collection = db.collection('teams'); // assign collection to var
-    // could return all of the items, but 'Bruins' is specified below
-    collection.find({teamName : 'Bruins'}).toArray(function(err, items) {
-      console.log(items);
+
+    /* find team with the name supplied */
+    collection.find({teamName : team}).toArray(function(err, items) {
+      teamLocation = items[0].location; // find location of team supplied
+      console.log(teamLocation);
     });
   }
 });
-
-/* Notes
-
-the mongo db wire protocol is built around four main operations:
-
-insert/update/remove/query
-
-the find() method
-
-- does not run the actual query, it builds an instance of a cursor that is then used to retrieve the data.  Simplest way is to use the toArray method, it is not good for enormous result sets, but it gets the job done.  It sends the entire document into the response:
-
-collection.find().toArray(function(err, items) {
-  console.log(items);
-});
-
-
-
- */
